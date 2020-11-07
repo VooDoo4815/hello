f62f7f6066e3f9e5fe7067dc667dfe06ec653032
Voodoo <killer4815@mail.ru>
2020-11-07 16:26:53 +0300

    port changed

--- src/main/kotlin/first/hello/HelloApplication.kt
+++ src/main/kotlin/first/hello/HelloApplication.kt
@@ -2,10 +2,14 @@ package first.hello
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class HelloApplication
 
 fun main(args: Array<String>) {
-	runApplication<HelloApplication>(*args)
+	runApplication<HelloApplication>(*args){
+		setDefaultProperties(Collections
+				.singletonMap("server.port", "8015") as Map<String, Any>?)
+	}
 }
